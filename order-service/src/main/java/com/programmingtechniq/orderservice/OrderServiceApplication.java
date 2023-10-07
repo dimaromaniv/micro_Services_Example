89a01389b58fd9b04f520ed5f20b7d4b977fec87
Dmytro Romaniv <dimaromaniv@gmail.com>
2023-10-07 14:21:59 -0400

    order services added

---
+++ order-service/src/main/java/com/programmingtechniq/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programmingtechniq.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+///safsafs
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
