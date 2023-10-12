d29d4daab0ed9268b3bb4a6671d5302a88d3ae60
Dmytro Romaniv <dimaromaniv@gmail.com>
2023-10-11 20:34:40 -0400

    add discovery server package

---
+++ discovery-server/src/test/java/com/programmingtechniq/discovery_server/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingtechniq.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
